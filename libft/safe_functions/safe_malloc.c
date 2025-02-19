9f1d7f036a5ad0431ca0cfa91bd14c0d6487c68a
Welepy-dev <mwsilva.contato@gmail.com>
2025-02-19 15:38:29 +0000

    small change

--- libft/safe_functions/safe_malloc.c
+++ libft/safe_functions/safe_malloc.c
@@ -6,7 +6,7 @@
 /*   By: welepy <welepy@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/11 13:39:48 by marcsilv          #+#    #+#             */
-/*   Updated: 2024/11/12 14:04:23 by welepy           ###   ########.fr       */
+/*   Updated: 2025/02/19 15:05:12 by welepy           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,9 +18,6 @@ void	*safe_malloc(size_t size)
 
 	ptr = malloc(size);
 	if (!ptr)
-	{
 		ft_putendl_fd("Malloc failed", 2);
-		exit(EXIT_FAILURE);
-	}
 	return (ptr);
 }
