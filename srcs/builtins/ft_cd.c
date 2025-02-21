874b6d3baba4bfe602e2e84db3a10b01f682d74a
Welepy-dev <mwsilva.contato@gmail.com>
2025-02-21 17:35:43 +0000

    Refactor built-in commands and improve error handling in ft_cd and ft_echo

--- srcs/builtins/ft_cd.c
+++ srcs/builtins/ft_cd.c
@@ -6,7 +6,7 @@
 /*   By: welepy <welepy@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/16 16:02:16 by mchingi           #+#    #+#             */
-/*   Updated: 2025/02/11 21:19:16 by welepy           ###   ########.fr       */
+/*   Updated: 2025/02/21 16:45:43 by welepy           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,11 @@ void	ft_cd(t_token *current)
 	char	*path;
 	char	*new_path;
 
+	if (current == NULL)
+	{
+		printf("cd: no arguments provided\n");
+		fprintf(stderr, "cd: too many arguments\n");
+	}
 	cur_path = getcwd(NULL, 0);
 	if (current->next)
 	{
@@ -29,7 +34,7 @@ void	ft_cd(t_token *current)
 	if (strcmp(current->value, "-") == 0)
 	{
 		if (!prev_path)
-			printf("cd: OLDPWD not set\n");
+			fprintf(stderr, "cd: OLDPWD not set\n");
 		else
 		{
 			if (chdir(prev_path) == 0)
@@ -45,14 +50,21 @@ void	ft_cd(t_token *current)
 	if (chdir(current->value) == 0)
 	{
 		printf("\nfrom built-ins\n");
+		free(cur_path);
 		return ;
 	}
 	path = ft_strjoin(cur_path, "/");
 	new_path = ft_strjoin(path, current->value);
+	free(path);
 	if (*current->value == '~')
+	{
+		free(new_path);
 		new_path = ft_strjoin(getenv("HOME"), current->value + 1);
+	}
 	if (chdir(new_path) == -1)
 		printf("cd: %s: No such file or directory\n", current->value);
+	free(new_path);
+	free(cur_path);
 	printf("\nfrom built-ins\n");
 }
 
